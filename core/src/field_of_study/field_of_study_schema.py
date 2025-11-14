a4827293dbb36df532e3df54da48b924a41e6fbf
mishuuw <katimerbo@gmail.com>
2025-11-14 10:46:07 +1000

    fix

--- core/src/field_of_study/field_of_study_schema.py
+++ core/src/field_of_study/field_of_study_schema.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field, constr
 class FieldOfStudySchema(BaseModel):
     code: str = Field(..., pattern=r'^[0-9]{2}\.[0-9]{2}\.[0-9]{2}$', description="Field of Study code")
     title: str = Field(..., description="Field of Study title")
-    title_short: str = Field(..., description="Field of Study short title")
+    title_short: str = Field(..., description="Field of Study short title", max_length=5)
     
 class FieldOfStudyGetSchema(FieldOfStudySchema):
     id: int = Field(..., description="Field of Study ID")
@@ -15,7 +15,7 @@ class FieldOfStudyUpdateSchema(BaseModel):
     id: int = Field(..., description="Field of Study ID")
     code: Optional[str] = Field(None, pattern=r'^[0-9]{2}\.[0-9]{2}\.[0-9]{2}$', description="Field of Study code")
     title: Optional[str] = Field(None, description="Field of Study title")
-    title_short: Optional[str] = Field(None, description="Field of Study short title")
+    title_short: Optional[str] = Field(None, description="Field of Study short title", max_length=5)
 
 class FieldOfStudyGetViewSchema(BaseModel):
     count: int = Field(..., description="Total count")
